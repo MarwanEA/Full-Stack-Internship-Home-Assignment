9291354c1873a1f6b723cb695abc5c5335eae219
MarwanEA <marwanelaoumrani@gmail.com>
2024-01-07 08:49:56 +0100

    Adding Changes

--- backend/src/main/java/ma/dnaengineering/backend/dto/ReportRequest.java
+++ backend/src/main/java/ma/dnaengineering/backend/dto/ReportRequest.java
@@ -1,2 +1,16 @@
-package ma.dnaengineering.backend.dto;public class ReportRequest {
+package ma.dnaengineering.backend.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ReportRequest {
+
+    private MultipartFile csvFile;
+
+    public MultipartFile getCsvFile() {
+        return csvFile;
+    }
+
+    public void setCsvFile(MultipartFile csvFile) {
+        this.csvFile = csvFile;
+    }
 }
--- backend/src/main/java/ma/dnaengineering/backend/entities/Employee.java
+++ backend/src/main/java/ma/dnaengineering/backend/entities/Employee.java
@@ -1,2 +1,19 @@
-package ma.dnaengineering.backend.entities;public class Employee {
+package ma.dnaengineering.backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Employee {
+
+    private Long id;
+
+    private String name;
+
+    private String jobTitle;
+
+    private Double salary;
 }
